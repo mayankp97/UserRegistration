02cbcca5e3aed59757e19b4cb802f29811dad3c2
Emma Paris <mayankpurohit2012@gmail.com>
2020-10-07 17:46:33 +0530

    [Mayank] Add Validation for password at least 1 number

--- User.cs
+++ User.cs
@@ -17,7 +17,7 @@ namespace UserRegistration
         private string _regexEmail = "^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
 
         public string password { get; set; }
-        private string _regexPassword = "^(?=.*[A-Z])[\\S]{8,}$";
+        private string _regexPassword = "^(?=.*[A-Z])(?=.*[0-9])[\\S]{8,}$";
 
         public string mobileNumber { get; set; }
         private string _regexMobileNumber = "^[0-9]{2}[ ][1-9][0-9]{9}$";

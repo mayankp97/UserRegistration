2ac2d792c5d036dc7b6b79a7736cdf1d015c3379
Emma Paris <mayankpurohit2012@gmail.com>
2020-10-07 12:37:16 +0530

    [Mayank] Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration!");
+
+            var user = new User();
+            Console.Write("Enter First Name :");
+            var firstName = Console.ReadLine();
+            if (user.ValidateFirstName(firstName))
+                user.firstName = firstName;
+            else
+                Console.WriteLine("Invalid first name. " +
+                    "It should have minimum 3 letters and only first letter as capital");
+
+
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class User
+    {
+        public string firstName { get; set; }
+        private string _regexFirstName = "^[A-Z][a-z]{2,}$";
+        public string lastName { get; set; }
+        private string _regexLastName = "";
+
+        public string email { get; set;  }
+        private string _regexEmail = "";
+
+        public string password { get; set; }
+        private string _regexPassword = "";
+
+        public string mobileNumber { get; set; }
+        private string _regexMobileNumber = "";
+
+
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, _regexFirstName);
+        }
+
+        
+    }
+}
